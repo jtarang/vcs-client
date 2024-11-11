44c146250af311bc4f39e83ba1952b834107570b
Jasmit Tarang <jaytarang92@gmail.com>
2024-11-11 13:11:47 -0600

    Update: examples, models, and client to use type in VCSConnectionObject

--- examples/github_gitlab_example.py
+++ examples/github_gitlab_example.py
@@ -6,16 +6,18 @@ if __name__ == "__main__":
     VCS_CLIENT = VCSClient()
 
     GITHUB_CONNECTION_OBJECT = VCSConnectionObject(
+        type=VCSNames.GITHUB,
         url='https://api.github.com',
         token='your_github_token'
     )
     GITLAB_CONNECTION_OBJECT = VCSConnectionObject(
+        type=VCSNames.GITLAB,
         url='https://gitlab.com/api/v4',
         token='your_gitlab_token'
     )
 
-    GITHUB_CLIENT = VCS_CLIENT.get_client(client_type=VCSNames.GITHUB, vcs_connection_object=GITHUB_CONNECTION_OBJECT)
-    GITLAB_CLIENT = VCS_CLIENT.get_client(client_type=VCSNames.GITLAB, vcs_connection_object=GITLAB_CONNECTION_OBJECT)
+    GITHUB_CLIENT = VCS_CLIENT.get_client(vcs_connection_object=GITHUB_CONNECTION_OBJECT)
+    GITLAB_CLIENT = VCS_CLIENT.get_client(vcs_connection_object=GITLAB_CONNECTION_OBJECT)
 
     GITHUB_REPOS = GITHUB_CLIENT.get_repos()
     GITLAB_REPOS = GITLAB_CLIENT.get_repos()
--- src/models.py
+++ src/models.py
@@ -1,8 +1,11 @@
 from dataclasses import dataclass
+from src.enums import VCSNames
+
 
 @dataclass
 class VCSConnectionObject:
     """ VCSConnectionObject: Contains all connetion info """
+    type: VCSNames
     url: str
     token: str
 
